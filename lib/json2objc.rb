32def897cc783402b10666e1a2122baa4f669b7f
Ivan Ostafiychuk <iostaf@gmail.com>
2013-07-30 12:39:23 +0300

    Add genmap implementation.

--- lib/json2objc.rb
+++ lib/json2objc.rb
@@ -1,20 +1,25 @@
 require "json2objc/version"
 require "thor"
+require "json"
+require "active_support/all"
 
 module Json2objc
   class CmdCLI < Thor
     desc "genprop input_file.json", "generate Objective-C class properties"
-    desc "genmap  input_file.json", "generate RestKit 0.20.2 class mapping"
-
     def genprop(input_json_file)
       result = ''
-      data = JSON.parse input_json_file
+      data = JSON.parse File.read(input_json_file)
       #data.keys.each { |k| result += "@\"#{k}\": @\"#{k.camelize(:lower)}\",\n" }
       data.keys.each { |k| result += "@property (nonatomic, strong) NSString *#{k.camelize(:lower)};\n" }
       puts result
     end
 
-    def genmap(input_file)
+    desc "genmap  input_file.json", "generate RestKit 0.20.2 class mapping"
+    def genmap(input_json_file)
+      result = ''
+      data = JSON.parse File.read(input_json_file)
+      data.keys.each { |k| result += "@\"#{k}\": @\"#{k.camelize(:lower)}\",\n" }
+      puts result
     end
   end
 end
