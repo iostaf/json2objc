0b1d3f81ad4c05d0bc706d5697f0df20e0dc3331
Ivan Ostafiychuk <iostaf@gmail.com>
2013-07-30 12:30:06 +0300

    Add objc props generation.

--- json2objc.gemspec
+++ json2objc.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = Json2objc::VERSION
   spec.authors       = ["Ivan Ostafiychuk"]
   spec.email         = ["iostaf@gmail.com"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
+  spec.description   = %q{JSON to Objective-C conversion helper}
+  spec.summary       = %q{JSON to Objective-C conversion helper}
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -20,4 +20,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "thor"
 end
--- lib/json2objc.rb
+++ lib/json2objc.rb
@@ -1,5 +1,22 @@
 require "json2objc/version"
+require "thor"
 
 module Json2objc
-  # Your code goes here...
+  class CmdCLI < Thor
+    desc "genprop input_file.json", "generate Objective-C class properties"
+    desc "genmap  input_file.json", "generate RestKit 0.20.2 class mapping"
+
+    def genprop(input_json_file)
+      result = ''
+      data = JSON.parse input_json_file
+      #data.keys.each { |k| result += "@\"#{k}\": @\"#{k.camelize(:lower)}\",\n" }
+      data.keys.each { |k| result += "@property (nonatomic, strong) NSString *#{k.camelize(:lower)};\n" }
+      puts result
+    end
+
+    def genmap(input_file)
+    end
+  end
 end
+
+Json2objc::CmdCLI.start(ARGV)
